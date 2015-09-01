6d22dfe229cf84c45a6ec9986bc2b4374abc9f1f
Manuel Zavatta <manuel.zavatta@gmail.com>
2015-09-01 14:35:46 +0200

    Release 1.0

---
+++ module.inc.php
@@ -0,0 +1,10 @@
+<?php
+/* -------------------------------------------------------------------------- *\
+|* -[ World - Module Information ]------------------------------------------- *|
+\* -------------------------------------------------------------------------- */
+$module_name="world";
+$module_version="1.0.0";
+$module_title="World";
+$module_description="World countries, regions, districts and cities";
+$module_create_menu=FALSE;
+?>
